a821dcc7d3d61e5aea2427fb35ccf2376a0c1172
Dmytro Shevtsov <shevtsov@adobe.com>
2023-11-21 15:15:40 -0600

    COMDOX-156: Add a check for external links on GitHub

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -1,23 +1,32 @@
 import remarkValidateLinks from "remark-validate-links";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkLintFrontmatterSchema from "remark-lint-frontmatter-schema";
+import remarkLintNoDeadUrls from "remark-lint-no-dead-urls";
 
 const remarkConfig = {
-  plugins: [
-    remarkValidateLinks,
-    remarkFrontmatter,
-    [
-      remarkLintFrontmatterSchema,
-      {
-        schemas: {
-          /* One schema for many files */
-          "./.github/linters/metadata.schema.yml": [
-            /* Support glob patterns ———v */
-            "./src/pages/**/*.md",
-          ],
-        },
-      },
-    ],
-  ],
+	plugins: [
+		remarkValidateLinks,
+		remarkFrontmatter,
+		[
+			remarkLintFrontmatterSchema,
+			{
+				schemas: {
+					/* One schema for many files */
+					"./.github/linters/metadata.schema.yml": [
+						/* Support glob patterns ———v */
+						"./src/pages/**/*.md",
+					],
+				},
+			},
+		],
+		[
+			remarkLintNoDeadUrls,
+			{
+				skipUrlPatterns: [
+					"https://www.php.net"
+				]
+			}
+		],
+	],
 };
 export default remarkConfig;
