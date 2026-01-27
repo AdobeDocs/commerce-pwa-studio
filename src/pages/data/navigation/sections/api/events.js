397deceb2b67507232a4b009f73596ae553edf9b
Dima Shevtsov <shevtsov@adobe.com>
2026-01-27 15:58:01 -0600

    Delete old navigation

--- src/pages/data/navigation/sections/api/events.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  title: "Events",
-  path: "/api/events",
- 
-};
--- src/pages/data/navigation/sections/index.js
+++
@@ -1,7 +0,0 @@
-const guides = require("./guides");
-const tutorials = require("./tutorials");
-const integrations = require("./integrations");
-const api = require("./api");
-const metapackages = require("./metapackages");
-
-module.exports = [...guides, ...integrations, ...tutorials, ...api, ...metapackages];
